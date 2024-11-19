1419248a953f38ce671ef2f69e835e7949865ac6
Egor Guzaev <enotguzaev604@gmail.com>
2024-11-19 14:10:32 +0300

    update
    добавить калибовку, оформить в целом лучше
    добавить регуляровку звука, а так же усложнить звук freq

--- src/effect.js
+++ src/effect.js
@@ -15,33 +15,61 @@ class Effect{
         const {ctx, canvas, video}=this;
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const locs=getLocationsWithColor(
-            imgData,{r:0,g:200,b:0}
+        const locs_freq=getLocationsWithColor(
+            imgData,{r:0,g:220,b:0}
         );
-        ctx.fillStyle="yellow";
-        locs.forEach(loc=>{
+        const locs_volume=getLocationsWithColor(
+            imgData,{r:0,g:0,b:220}
+        );
+        ctx.fillStyle="green";
+        
+        locs_freq.forEach(loc=>{
             ctx.fillRect(loc.x,loc.y,1,1);
         });
-        if(locs.length>0){
-            const center=average(locs);
-            const p=1-center.y/canvas.height;
+        ctx.fillStyle="blue";
+        locs_volume.forEach(loc=>{
+            ctx.fillRect(loc.x,loc.y,1,1);
+        });
+        if(locs_freq.length>0){
+        //freq
+            const center_freq=average(locs_freq);
+            const p=1-center_freq.y/canvas.height;
             const freq=200+500*p;
             this.osc.frequency.value=freq;
 
-
             ctx.beginPath();
-            ctx.fillStyle="red";
-            ctx.arc(center.x,center.y,5,0,2*Math.PI);
+            ctx.fillStyle_freq="red";
+            ctx.arc(center_freq.x,center_freq.y,5,0,2*Math.PI);
             ctx.fill();
             ctx.beginPath();
             ctx.strokeStyle = "green";
             ctx.lineWidth = 5;
-            ctx.moveTo(0,center.y);
-            ctx.lineTo(canvas.width,center.y);
+            ctx.moveTo(0,center_freq.y);
+            ctx.lineTo(canvas.width,center_freq.y);
             ctx.stroke();
         }else{
             this.osc.frequency.value=0;
 
+        }
+        if(locs_volume.length>0){
+        //volume_test
+            const center_volume=average(locs_volume);
+            const p_volume=1-center_volume.y/canvas.height;
+            //const freq=200+500*p;
+            //this.osc.frequency.value=freq;
+
+            ctx.beginPath();
+            ctx.fillStyle_volume="red";
+            ctx.arc(center_volume.x,center_volume.y,5,0,2*Math.PI);
+            ctx.fill();
+            ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.lineWidth = 5;
+            ctx.moveTo(0,center_volume.y);
+            ctx.lineTo(canvas.width,center_volume.y);
+            ctx.stroke();
+        }else{
+        //volume =0
         }
         requestAnimationFrame(this.#animate.bind(this));
     }
