9e96eb3542e0c7f3f354ea950170e5ac1c6c8c5a
Egor-Guzaev <enotguzaev604@gmail.com>
2024-11-19 21:53:10 +0300

    чето переделал
    добавить калибовку, оформить в целом лучше, в пункте настроек
    попробовать сделать композиции тренировку
    и дизайн сайта
    сдеалано 2ой маркер, громкость, частота, фильтры

--- src/effect.js
+++ src/effect.js
@@ -1,76 +1,85 @@
-class Effect{
-    constructor(canvas,video) {
+class Effect {
+    constructor(canvas, video) {
         this.canvas = canvas;
         this.video = video;
         this.ctx = this.canvas.getContext('2d');
-        const audioCtx =new AudioContext();
-        this.osc=audioCtx.createOscillator();
-        this.osc.connect(audioCtx.destination);
-        this.osc.frequency.value = 0;
+
+        const audioCtx = new AudioContext();
+        this.osc = audioCtx.createOscillator();
+        this.gainNode = audioCtx.createGain();
+        this.filterNode = audioCtx.createBiquadFilter(); // Фильтр для сглаживания тона
+
+        // Настройка фильтра
+        this.filterNode.type = 'lowpass';
+        this.filterNode.frequency.value = 1000; // Частота среза фильтра
+        this.filterNode.Q.value = 1; // Резонанс
+
+        // Подключение узлов
+        this.osc.connect(this.filterNode).connect(this.gainNode).connect(audioCtx.destination);
+
+        // Настройка осциллятора
+        this.osc.type = 'triangle'; // Мягкий тон
+        this.osc.frequency.value = 0; // Частота по умолчанию
+        this.gainNode.gain.value = 0; // Громкость по умолчанию
         this.osc.start();
 
         this.#animate();
     }
-    #animate(){
-        const {ctx, canvas, video}=this;
+
+    #animate() {
+        const { ctx, canvas, video } = this;
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const locs_freq=getLocationsWithColor(
-            imgData,{r:0,g:220,b:0}
-        );
-        const locs_volume=getLocationsWithColor(
-            imgData,{r:0,g:0,b:220}
-        );
-        ctx.fillStyle="green";
-        
-        locs_freq.forEach(loc=>{
-            ctx.fillRect(loc.x,loc.y,1,1);
-        });
-        ctx.fillStyle="blue";
-        locs_volume.forEach(loc=>{
-            ctx.fillRect(loc.x,loc.y,1,1);
-        });
-        if(locs_freq.length>0){
-        //freq
-            const center_freq=average(locs_freq);
-            const p=1-center_freq.y/canvas.height;
-            const freq=200+500*p;
-            this.osc.frequency.value=freq;
+
+        const locs_freq = getLocationsWithColor(imgData, { r: 0, g: 230, b: 0 }); // Частота
+        const locs_volume = getLocationsWithColor(imgData, { r: 0, g: 0, b: 230 }); // Громкость
+
+        // Управление частотой
+        if (locs_freq.length > 0) {
+            const center_freq = average(locs_freq);
+            const p = center_freq.x / canvas.width;
+            const freq = 200 + 500 * p;
+            this.osc.frequency.value = freq;
+
+            // Управление фильтром по вертикальной позиции
+            const filterCutoff = 500 + 2000 * (1 - center_freq.y / canvas.height);
+            this.filterNode.frequency.value = filterCutoff;
 
             ctx.beginPath();
-            ctx.fillStyle_freq="red";
-            ctx.arc(center_freq.x,center_freq.y,5,0,2*Math.PI);
+            ctx.fillStyle = "red";
+            ctx.arc(center_freq.x, center_freq.y, 5, 0, 2 * Math.PI);
             ctx.fill();
             ctx.beginPath();
             ctx.strokeStyle = "green";
             ctx.lineWidth = 5;
-            ctx.moveTo(0,center_freq.y);
-            ctx.lineTo(canvas.width,center_freq.y);
+            ctx.moveTo(center_freq.x, 0);
+            ctx.lineTo(center_freq.x, canvas.height);
             ctx.stroke();
-        }else{
-            this.osc.frequency.value=0;
-
+        } else {
+            this.osc.frequency.value = 0;
         }
-        if(locs_volume.length>0){
-        //volume_test
-            const center_volume=average(locs_volume);
-            const p_volume=1-center_volume.y/canvas.height;
-            //const freq=200+500*p;
-            //this.osc.frequency.value=freq;
+
+        // Управление громкостью
+        if (locs_volume.length > 0) {
+            const center_volume = average(locs_volume);
+            const p_volume = 1 - center_volume.y / canvas.height;
+            const volume = Math.min(Math.max(p_volume, 0), 1);
+            this.gainNode.gain.value = volume;
 
             ctx.beginPath();
-            ctx.fillStyle_volume="red";
-            ctx.arc(center_volume.x,center_volume.y,5,0,2*Math.PI);
+            ctx.fillStyle = "red";
+            ctx.arc(center_volume.x, center_volume.y, 5, 0, 2 * Math.PI);
             ctx.fill();
             ctx.beginPath();
             ctx.strokeStyle = "blue";
             ctx.lineWidth = 5;
-            ctx.moveTo(0,center_volume.y);
-            ctx.lineTo(canvas.width,center_volume.y);
+            ctx.moveTo(0, center_volume.y);
+            ctx.lineTo(canvas.width, center_volume.y);
             ctx.stroke();
-        }else{
-        //volume =0
+        } else {
+            this.gainNode.gain.value = 0;
         }
+
         requestAnimationFrame(this.#animate.bind(this));
     }
 }
